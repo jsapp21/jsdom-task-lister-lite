1f2fc56356fff838670d7db50106484ee8297bae
James Sapp <jameseisapp@gmail.com>
2021-02-02 12:06:29 -0600

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const tasks = document.getElementById('tasks')
+const text = document.getElementById('new-task-description')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', logSubmit);
 });
+
+function logSubmit(event) {
+  let element = document.createElement('li')
+  tasks.appendChild(element)
+  let newTask = tasks.lastChild
+  newTask.innerText = text.value
+  event.preventDefault();
+}
+
