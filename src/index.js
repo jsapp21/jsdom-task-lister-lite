20a1efd56314f27434a63a5f3dc128fdb1e5ac4b
James Sapp <jameseisapp@gmail.com>
2021-02-02 12:12:26 -0600

    solution w/notes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+// these variables need to be outside that locate the html id's
 const tasks = document.getElementById('tasks')
 const text = document.getElementById('new-task-description')
 
+// event listner for dom, form and submit
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById('create-task-form');
   form.addEventListener('submit', logSubmit);
 });
 
+// upon submit create li for the tasks variable, 
+// appened it to the tasks
+// new task variable call last child and insert the text value
+// prevent the event from submitting
 function logSubmit(event) {
   let element = document.createElement('li')
   tasks.appendChild(element)
